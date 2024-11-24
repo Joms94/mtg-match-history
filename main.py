2bc98c557eb2165dd1d20888f0b4c48efe1adefb
Joms94 <davisjamesharford@gmail.com>
2024-11-24 17:38:06 +1030

    feat: Add Google Sheets connectivity.
    Adds a simple module for handling Google Cloud
    authentication and reading data from sheets. This
    commit also adds support for loading environment
    variables from a .env file. Good for safety
    and convenience when dealing with API secrets.
    
    I decided it was worth writing this because it's
    a bit tedious to manually export csvs whenever
    I want to load stuff into my database. I thought
    I'd go on prioritising other things, but this
    will speed up everything else.

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 """Main file for launching the dashboard."""
 
+
 import os
+import ast
+
 from dotenv import load_dotenv
+
+from src.gsheets_interaction import GoogleSheet
+
+
+def init() -> dict:
+    """Get environment variables. These are primarily
+    used to configure and use the Google Cloud API.
+    See gsheets_interaction.py for details."""
+    load_dotenv()
+    return {
+        "GSHEET_ID": os.getenv("GSHEET_ID"),
+        "CREDS_FPATH": os.getenv("CREDS_FPATH"),
+        "RANGE_NAME": os.getenv("RANGE_NAME"),
+        "SCOPES": ast.literal_eval(os.getenv("SCOPES"))
+    }
+
+
+def main():
+    """Get result from Google API."""
+    config = init()
+    sheet = GoogleSheet(config["CREDS_FPATH"], config["SCOPES"])
+    print(sheet.get_data(config["GSHEET_ID"], config["RANGE_NAME"]))
 
 
 if __name__ == "__main__":
-    print(os.environ)
+    main()
---
+++ src/gsheets_interaction.py
@@ -0,0 +1,43 @@
+"""Module responsible for configuring Google Cloud
+API connections, pulling sheet data, then coercing
+it into a more useable format."""
+
+
+from googleapiclient.discovery import build, Resource
+from google.oauth2.service_account import Credentials
+
+
+class GoogleSheet:
+    """Authenticates with the Google Cloud API
+    and gets the contents of a sheet.
+    
+    creds_fpath: a .json supplied in the Google
+    Cloud developer portal when you set up a
+    service account for your project.
+
+    scopes: OAuth 2 scopes required by Google.
+    Even when you're using a service account,
+    this still seems necessary. For more info:
+    https://developers.google.com/identity/protocols/oauth2/scopes"""
+    def __init__(self, creds_fpath: str, scopes: str) -> None:
+        self.creds_fpath = creds_fpath
+        self.scopes = scopes
+        self.creds = Credentials.from_service_account_file(creds_fpath, scopes=scopes)
+
+    def get_data(self, gsheet_id: str, range_name: str) -> dict[str, list[]]:
+        """Extract data from a sheet in your
+        Google Sheets workbook..
+        
+        range_name: The name of a range specified
+        in the same manner as you would within
+        a typical Sheets formula. E.g., if you
+        were trying to access the first column
+        and first 10 rows of a sheet, this might
+        be Sheet1!A1:A10, substituting 'Sheet1'
+        with your actual sheet name.
+        
+        gsheet_id: There's a jumble of characters
+        in the URL for every GSheet between '/d/'
+        and '/edit'. This is your gsheet_id."""
+        with build('sheets', 'v4', credentials=self.creds) as service:
+            return service.spreadsheets().values().get(spreadsheetId=gsheet_id, range=range_name).execute()
