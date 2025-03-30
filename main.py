a6d8b31117c2b8c84062b18de70a9f56227141ef
Joms94 <davisjamesharford@gmail.com>
2025-03-30 14:19:14 +1030

    misc: Clear redundant Supabase associations.
    Remove redundant cleaning functions and other
    debris from main. Rather than transform the data
    in anticipation of a pre-defined load process,
    the philosophy will now be to keep the data as close
    to raw as possible for a staging load into DuckDB.

--- main.py
+++ main.py
@@ -3,12 +3,18 @@
 
 import os
 import ast
+import json
 
 from dotenv import load_dotenv
 
 from src.gsheets_interaction import GoogleSheet
-from src.data_cleaning import reformat_api_values_for_postgres
-from src.postgres_interaction import PostgresDB
+
+
+# .env file parameter names. Used for authentication.
+GSHEET = "GSHEET_ID"
+GCREDS = "GOOGLE_CREDS_FPATH"
+GRANGE = "RANGE_NAME"
+SCOPES = "SCOPES"
 
 
 def init() -> dict:
@@ -17,27 +23,20 @@ def init() -> dict:
     load_dotenv()
     return {
         # Google Sheets.
-        "GSHEET_ID": os.getenv("GSHEET_ID"),
-        "GOOGLE_CREDS_FPATH": os.getenv("GOOGLE_CREDS_FPATH"),
-        "RANGE_NAME": os.getenv("RANGE_NAME"),
-        "SCOPES": ast.literal_eval(os.getenv("SCOPES")),
-        # Cloud Postgres instance.
-        "SUPABASE_URL": os.getenv("SUPABASE_URL"),
-        "SUPABASE_KEY": os.getenv("SUPABASE_KEY")
+        GSHEET: os.getenv(GSHEET),
+        GCREDS: os.getenv(GCREDS),
+        GRANGE: os.getenv(GRANGE),
+        SCOPES: ast.literal_eval(os.getenv(SCOPES))
     }
 
 
 def main():
     """Get result from Google API."""
     config = init()
-    # sheet = GoogleSheet(config["GOOGLE_CREDS_FPATH"], config["SCOPES"])
-    # data = sheet.get_data(config["GSHEET_ID"], config["RANGE_NAME"])
-    # cleaned_data = reformat_api_values_for_postgres(data["values"])
-    with open("gsheet_output.txt", mode="r", encoding="utf-8") as mock_data:
-        cleaned_data = ast.literal_eval(mock_data.read())
-    db = PostgresDB(url=config["SUPABASE_URL"], key=config["SUPABASE_KEY"])
-    db.insert_into_table(data=cleaned_data)
-    # print(db.reinitialise_tables())
+    sheet = GoogleSheet(config[GCREDS], config[SCOPES])
+    data = sheet.get_data(config[GSHEET], config[GRANGE])
+    with open("gsheet_output.json", mode="w+", encoding="utf-8") as output_file:
+        json.dump(data, output_file)
 
 
 if __name__ == "__main__":
--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -9,24 +9,28 @@ from src.data_cleaning import (
 )
 
 
-def test_reformat_values_df(google_api_values):
+def test_reformat_values_df(google_api_values, capsys):
     """Figure out if we're correctly converting
     the list of lists supplied by the Google
     Cloud API into a dictionary consisting of
     column headers as keys and rows as values."""
     keys_to_check = google_api_values[0]
     data = reformat_api_values_for_df(google_api_values)
+    with capsys.disabled():
+        print("test_reformat_values_df 'data' var: ", data, "\n") # Print regardless of outcome.
     assert all(key in data.keys() for key in keys_to_check)
     assert all(len(data[key]) == 2 for key in keys_to_check)
 
 
-def test_reformat_values_postgres(google_api_values):
+def test_reformat_values_postgres(google_api_values, capsys):
     """Test whether the Google Sheets data
     reformatting function correctly converts
     the format to that expected by the
     Supabase API."""
     keys_to_check = google_api_values[0]
     rows = reformat_api_values_for_postgres(google_api_values)
+    with capsys.disabled():
+        print("test_reformat_values_postgres 'rows' var: ", rows, "\n") # Print regardless of outcome.
     for row in rows:
         # Check key existence.
         assert all(key in row.keys() for key in keys_to_check)
